525a66b99cd32c5991f0dd6fa1e6c2451d8ea0d8
NaZiiru <m.naziroudiankha@gmail.com>
2024-10-11 00:15:46 +0000

    cors configuration, use cookieParser

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const connectDB = require('./config/mongoose');
 const config = require('../config');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express();
 
 connectDB();
 
+
+app.use(
+  cors({
+    origin : ' http://localhost:5173/',
+    method: ['GET', 'POST', 'DELETE', 'PUT', 'PATCH'],
+    allowedHeaders : [
+      'Content-Type',
+      'Authorization',
+      'Cache-Control',
+      'Expires',
+      'Pragma'
+    ],
+    credentials: true
+  })
+)
+
+app.use(cookieParser());
 app.use(express.json());
 
 const PORT = config.port;
-   app.listen(PORT, () => {
-     console.log(`Server running on http://localhost:${PORT}`);
-   }); 
+
+app.listen(PORT, () => { console.log(`Server running on http://localhost:${PORT}`);});
