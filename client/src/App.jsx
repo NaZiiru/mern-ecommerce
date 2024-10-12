1aad6007e0862ae506afbdeced5777ec8d3010fd
NaZiiru <m.naziroudiankha@gmail.com>
2024-10-12 01:26:39 +0000

    admin layout route

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,31 @@
-import { Route, Routes } from "react-router-dom"
-import AuthLayout from "./components/auth/layout"
-import Login from "./pages/auth/login"
-import Register from "./pages/auth/register"
-
+import { Route, Routes } from "react-router-dom";
+import AuthLayout from "./components/auth/layout";
+import Login from "./pages/auth/login";
+import Register from "./pages/auth/register";
+import AdminLayout from "./components/admin/layout";
+import AdminDashboard from "./pages/admin/dashboard";
+import AdminProducts from "./pages/admin/products";
+import AdminOrders from "./pages/admin/orders";
+import AdminFeatures from "./pages/admin/features";
 
 const App = () => {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <Routes>
-        <Route path="/auth" element={<AuthLayout/>}>
-          <Route path="login" element={<Login/>}/>
-          <Route path="register" element={<Register/>}/>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="orders" element={<AdminOrders />} />
+          <Route path="features" element={<AdminFeatures />} />
         </Route>
       </Routes>
     </div>
   );
-}
+};
 
 export default App;
