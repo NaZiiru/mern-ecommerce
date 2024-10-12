aae447ec5fd71fc36d56daa53de8b4b9b9536e13
NaZiiru <m.naziroudiankha@gmail.com>
2024-10-12 02:17:08 +0000

    add shopping layout and routing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,21 +7,37 @@ import AdminDashboard from "./pages/admin/dashboard";
 import AdminProducts from "./pages/admin/products";
 import AdminOrders from "./pages/admin/orders";
 import AdminFeatures from "./pages/admin/features";
+import ShoppingLayout from "./components/shopping/layout";
+import NotFound from "./pages/not-found";
+import ShoppingHome from "./pages/shopping/home";
+import ShoppingProducts from "./pages/shopping/products";
+import ShoppingCheckout from "./pages/shopping/checkout";
+import ShoppingAccount from "./pages/shopping/account";
 
 const App = () => {
   return (
     <div className="flex flex-col overflow-hidden bg-white">
       <Routes>
+        <Route path="/" element={<ShoppingHome />} />
+        <Route path="*" element={<NotFound />} />
+        {/* authentification router */}
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
-
+        {/* admin router */}
         <Route path="/admin" element={<AdminLayout />}>
           <Route path="dashboard" element={<AdminDashboard />} />
           <Route path="products" element={<AdminProducts />} />
           <Route path="orders" element={<AdminOrders />} />
           <Route path="features" element={<AdminFeatures />} />
+        </Route>
+        {/* shop router */}
+        <Route path="/shop" element={<ShoppingLayout />}>
+          <Route path="home" element={<ShoppingHome />} />
+          <Route path="products" element={<ShoppingProducts />} />
+          <Route path="checkout" element={<ShoppingCheckout />} />
+          <Route path="account" element={<ShoppingAccount />} />
         </Route>
       </Routes>
     </div>
